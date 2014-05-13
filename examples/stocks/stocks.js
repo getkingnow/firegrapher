96ac83d675e9a37e27d5d6a877c0299ecbb127bd
jacobawenger <jacob@firebase.com>
2014-05-13 13:50:02 -0700

    Added more AAPL stocks and FireGrapher skeleton

--- examples/stocks/stocks.js
+++ examples/stocks/stocks.js
@@ -13,7 +13,8 @@
         { "time": 7, "price": 65.8 },
         { "time": 8, "price": 66 },
         { "time": 9, "price": 67 },
-        { "time": 10, "price": 70 }
+        { "time": 10, "price": 70 },
+        { "time": 11, "price": 75 }
       ],
       "AAPL": [
         { "time": 0, "price": 68 },
@@ -26,7 +27,27 @@
         { "time": 7, "price": 56 },
         { "time": 8, "price": 58 },
         { "time": 9, "price": 61 },
-        { "time": 10, "price": 67 }
+        { "time": 10, "price": 67 },
+        { "time": 11, "price": 68 },
+        { "time": 12, "price": 60 },
+        { "time": 13, "price": 56 },
+        { "time": 14, "price": 55.9 },
+        { "time": 15, "price": 53 },
+        { "time": 16, "price": 50 },
+        { "time": 17, "price": 55 },
+        { "time": 18, "price": 56 },
+        { "time": 19, "price": 58 },
+        { "time": 20, "price": 61 },
+        { "time": 21, "price": 60 },
+        { "time": 22, "price": 56 },
+        { "time": 23, "price": 55.9 },
+        { "time": 24, "price": 53 },
+        { "time": 25, "price": 50 },
+        { "time": 26, "price": 55 },
+        { "time": 27, "price": 56 },
+        { "time": 28, "price": 58 },
+        { "time": 29, "price": 61 },
+        { "time": 30, "price": 67 }
       ],
       "GOOG": [
         { "time": 0, "price": 100 },
