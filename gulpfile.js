11e923a086b0e3851d5f560b817692915f8b4a9d
David <david@firebase.com>
2014-05-15 14:28:57 -0700

    Gulp serve task.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,15 @@ var concat = require("gulp-concat");
 var jshint = require("gulp-jshint");
 var uglify = require("gulp-uglify");
 
+var express = require("express");
+var browserify = require('gulp-browserify');
+var livereload = require('connect-livereload');
+var refresh = require('gulp-livereload');
+var lrserver = require('tiny-lr')();
+var notify = require('gulp-notify');
+var livereloadport = 35729;
+var serverport = 3000;
+
 /****************/
 /*  FILE PATHS  */
 /****************/
@@ -23,6 +32,14 @@ var paths = {
     "tests/FireGrapher.spec.js"
   ]
 };
+
+
+//Configure server, but only start it when running the serve task
+var server = express();
+//Add livereload middleware before static-middleware
+server.use(livereload({
+  port: livereloadport
+}));
 
 /***********/
 /*  TASKS  */
@@ -69,10 +86,28 @@ gulp.task("test", function() {
     });
 });
 
+/* Sets up Browserify */
+gulp.task('browserify', function(){
+  gulp.src('build/*.js')
+   .pipe(browserify())
+   .pipe(refresh(lrserver));
+});
+
+gulp.task('server', function() {
+  //Set up your static fileserver, which serves files in the build dir
+  server.use(express.static(__dirname));
+  server.listen(serverport);
+  //Set up your livereload server
+  lrserver.listen(livereloadport);
+});
+
 /* Re-runs the "scripts" task every time a script file changes */
 gulp.task("watch", function() {
   gulp.watch(paths.scripts, ["scripts"]);
+  gulp.watch(["examples/**/*", "build/*.js"], ["browserify"]);
 });
 
+gulp.task("serve", ["scripts", "watch", "server"]);
+
 /* Runs the "test" and "scripts" tasks by default */
-gulp.task("default", ["test", "scripts"]);
+gulp.task("default", ["scripts", "test"]);
